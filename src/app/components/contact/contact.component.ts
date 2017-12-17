62afd866dff99e9e8190ff6a8964f7ca310ef5fc
Gopalakrishnan Subramani <krish@Gopalakrishnans-MacBook-Pro.local>
2017-12-17 11:27:41 +0530

    elvis operator

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  //# reference variable
+  //.? elvis operator null check
+  // [()] two way binding
+
+  address: any = undefined; //null
+
   constructor() { }
 
   ngOnInit() {
+     setTimeout ( () => {
+       this.address = {
+         city: 'BLR'
+       }
+     }, 2000);
   }
 
 }
